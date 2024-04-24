2ee7f0cf27ceac68857d6ef3c748a751cf20a3d4
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-04-24 22:44:59 +0200

    Add files via upload

---
+++ AdRenalen/plugins/additions/المطور.py
@@ -0,0 +1,86 @@
+import asyncio
+import os
+import time
+import requests
+import aiohttp
+import config
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from AdRenalen import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AdRenalen import app
+
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OWNER_ID = getenv("OWNER_ID")
+
+OWNER = getenv("OWNER")
+
+
+def get_file_id(msg: Message):
+    if msg.media:
+        for message_type in (
+            "photo",
+            "animation",
+            "audio",
+            "document",
+            "video",
+            "video_note",
+            "voice",
+            # "contact",
+            # "dice",
+            # "poll",
+            # "location",
+            # "venue",
+            "sticker",
+        ):
+            obj = getattr(msg, message_type)
+            if obj:
+                setattr(obj, "message_type", message_type)
+                return obj
+
+@app.on_message(filters.command(["المطور","مطور البوت"], ""))
+async def khfzss(client: Client, message: Message):
+    usrr = await client.get_chat(OWNER_ID)
+    name = usrr.first_name
+    bio = usrr.bio
+    id = usrr.id
+    username = usrr.username
+    async for photo in client.get_chat_photos(OWNER_ID, limit=1):
+                    await message.reply_photo(photo.file_id,       caption=f"""-⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝐎𝑳𝐈𝐕𝐄𝐀⌯.: \n\n⌔︙𝒏𝒂𝒎𝒆: {name} \n\n⌔︙𝒖𝒔𝒆𝒓: @{username} \n\n⌔︙𝒊𝒅: {id} \n\n⌔︙𝒃𝒊𝒐: {bio} \n\n⌔︙𝒋𝒐𝒃:  مطور البوت """, 
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        name, url=f"https://t.me/{username}")
+                ],
+            ]
+        ),
+    )                    
+                    sender_id = message.from_user.id
+                    sender_name = message.from_user.first_name
+                    await app.send_message(OWNER_ID, f"الواد {message.from_user.mention} دا بينادي عليك \n\n الايدي بتاعه : {sender_id} \n\n اسمه : {sender_name}")
+                    return await app.send_message(config.LOG_GROUP_ID, f"الواد {message.from_user.mention} دا بينادي عليك \n\n الايدي بتاعه : {sender_id} \n\n اسمه : {sender_name}")
+
+
+@app.on_message(filters.command(["تحويل لصوره"], ""))
+async def elkatifh(client: Client, message: Message):
+    reply = message.reply_to_message
+    if not reply:
+        return await message.reply("الرد على ملصق.")
+    if not reply.sticker:
+        return await message.reply("الرد على ملصق.")
+    m = await message.reply("يتم المعالجه..")
+    f = await reply.download(f"{reply.sticker.file_unique_id}.png")
+    await gather(*[message.reply_photo(f),message.reply_document(f)])
+    await m.delete()
+    os.remove(f)
+
+
+
---
+++ AdRenalen/plugins/additions/حذف.py
@@ -0,0 +1,30 @@
+import asyncio
+import os
+import time
+import requests
+import aiohttp
+from pyrogram import filters
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+from AdRenalen import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AdRenalen import app
+
+
+
+
+########################### بوت حذف
+@app.on_message(filters.command(["حذف", "عاوز احذف", "عاوزه احذف"], ""))
+async def svksksa(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph//file/a6137caa707bdb1247d7c.jpg",
+        caption=f"""[خش احذف محدش هيمسك فيك يلا غور فداهية 😂❤""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• اضـغـط لـدخول للـبوت", url=f"https://t.me/d_accubot")
+                ]
+           ]
+        ),
+    )
+   
