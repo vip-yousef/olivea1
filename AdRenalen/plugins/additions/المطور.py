3d5f0188b4cad79d0317e92321cd10821fad8a3d
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-04-30 00:28:41 +0300

    Update المطور.py

--- AdRenalen/plugins/additions/المطور.py
+++ AdRenalen/plugins/additions/المطور.py
@@ -53,7 +53,7 @@ async def khfzss(client: Client, message: Message):
     id = usrr.id
     username = usrr.username
     async for photo in client.get_chat_photos(OWNER_ID, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""-⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝐎𝑳𝐈𝐕𝐄𝐀⌯.: \n\n⌔︙𝒏𝒂𝒎𝒆: {name} \n\n⌔︙𝒖𝒔𝒆𝒓: @{username} \n\n⌔︙𝒊𝒅: {id} \n\n⌔︙𝒃𝒊𝒐: {bio} \n\n⌔︙𝒋𝒐𝒃:  مطور البوت """, 
+                    await message.reply_photo(photo.file_id,       caption=f"""-⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝐎𝑳𝐈𝐕𝐄𝐀⌯.: \n✯︙𝙽𝙰𝙼𝙴 : {name} \n✯︙𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴 : @{username} \n⌔✯︙𝙸𝙳 : {id} \n✯︙𝙱𝙸𝙾 : {bio} \n⌔︙𝙹𝙾𝙱 : مبرمج السورس """, 
         reply_markup=InlineKeyboardMarkup(
             [
                 [
