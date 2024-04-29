9835e20caf96565b774f36ed64df1fc06da3a623
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-04-29 21:02:15 +0300

    Update المالك.py

--- AdRenalen/plugins/play/المالك.py
+++ AdRenalen/plugins/play/المالك.py
@@ -12,8 +12,7 @@ async def instatus(app, message):
   async for owner in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
     if owner.status == enums.ChatMemberStatus.OWNER:
       saidi = app.get_users(owner.user.id)
-      bio = app.get_chat(owner.user.id).bio
       JABWA = InlineKeyboardMarkup([[InlineKeyboardButton(saidi.first_name, url=f"https://t.me/{saidi.username}")]])
       async for x in c.get_chat_photos(saidi.id, limit=1):
         photo = x.file_id
-      await m.message(photo,caption=f"**• name : [{saidi.first_name}](tg://user?id={saidi.id})\n• user : [@{saidi.username}]\n• id : {saidi.id}\n• bio : {bio}**",reply_markup=JABWA,quote=True)
+      await m.message(photo,caption=f"**• name : [{saidi.first_name}](tg://user?id={saidi.id})\n• user : [@{saidi.username}]\n• id : {saidi.id}**",reply_markup=JABWA,quote=True)
