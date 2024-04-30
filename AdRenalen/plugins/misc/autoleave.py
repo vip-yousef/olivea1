6d64dfc4d01607219bc4682bc378f5765d72a0f4
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-04-30 23:19:43 +0300

    Update autoleave.py

--- AdRenalen/plugins/misc/autoleave.py
+++ AdRenalen/plugins/misc/autoleave.py
@@ -26,8 +26,8 @@ async def auto_leave():
                         ]:
                             if (
                                 i.chat.id != config.LOGGER_ID
-                                and i.chat.id != -1002116081436
-                                and i.chat.id != -1002116081436
+                                and i.chat.id != -1002093464613
+                                and i.chat.id != -1002093464613
                             ):
                                 if left == 20:
                                     continue
