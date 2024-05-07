f1acbeac108ed6f8314bd50c6dafbe6e1d9a4c93
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-05-07 03:53:58 +0300

    Add files via upload

---
+++ AdRenalen/plugins/additions/مميزات..py
@@ -0,0 +1,32 @@
+import random
+from AarohiX import app
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.enums import ChatMemberStatus
+
+
+def Who(m, user_id):
+  user = m.chat.get_member(user_id)
+  if user.status == ChatMemberStatus.OWNER:
+    return "المالك"
+  elif user.status == ChatMemberStatus.ADMINISTRATOR:
+    return "مشرف"
+  elif user.status == ChatMemberStatus.MEMBER:
+    return "العضو"
+#####==> By JABWA <==#####
+@app.on_message(filters.command("كشف", "") & filters.group)
+def jabwa(c, m):
+  name = m.reply_to_message.from_user.first_name
+  id = m.reply_to_message.from_user.id
+  user = m.reply_to_message.from_user.username
+  rank = f"{Who(m,m.reply_to_message.from_user.id)}"
+  money = random.randint(1, 100)
+  Text =f"""**
+• الاسم : {name}
+• الايدي : {id}
+• اليوزر : {user}
+• الرتبه {rank}
+• سعر الكشف : {money} جنيه 😂❤️
+**"""
+  return m.reply(Text)
+
