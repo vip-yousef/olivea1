b6b617693e0a730d2b0fc8fab7d1572e3dbd52e3
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-05-10 19:44:23 +0300

    Update صورتي.py

--- AdRenalen/plugins/additions/صورتي.py
+++ AdRenalen/plugins/additions/صورتي.py
@@ -1,46 +1,56 @@
-import asyncio
 from pyrogram import Client, filters
+from pyrogram.enums import ChatMemberStatus
 from random import choice
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import (InlineKeyboardButton,CallbackQuery,
-                            InlineKeyboardMarkup, Message)
+from pyrogram.types import (InlineKeyboardButton,CallbackQuery,InlineKeyboardMarkup, Message)
 from AdRenalen import app
 from typing import Union
 from pyrogram.types import InlineKeyboardButton
 
-RAEAK = ["فاجره","حلوه","فخامه","جميله","خوش","جميله","يععععع","وحشه","مش حلوه","حلوه ياعم","خليك بيها","حبتها","غيرها يعم"]
 
-@app.on_message(filters.command(["صورتي"], ""))
-async def madison(client: Client, message: Message):
+stayle_pic = []
+The_Stayle_Pic = ["صورتك رايقة يصحبي","صورتك حلوة شوية","اي وشك دا يقطع الخميرة من البيت","تلاق تلاتة اتخديت من مالك","صورتك دي ولا القمر","عايز اتصور صورة معاك","انتا فجمال جورجينا","متحولش تغريني بغمزاتك","صورتك تشبه القمر"]
+
+
+@app.on_message(filters.command(["تعطيل صورتي", "قفل صورتي"], "") & filters.group)
+async def iddlock(client: Client, message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id in stayle_pic:
+            return await message.reply_text("امر صورتي معطل من قبل  💎 ،")
+        stayle_pic.append(message.chat.id)
+        return await message.reply_text("تم تعطيل امر صورتي بنجاح 💎 ،")
+    else:
+        return await message.reply_text("عذرا عزيزي هذا الامر للمشرفين بس 💎 ،")
+
+@app.on_message(filters.command(["فتح صورتي", "تفعيل صورتي"], "") & filters.group)
+async def iddopen(client: Client, message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id not in stayle_pic:
+            return await message.reply_text("امر صورتي مفعل من قبل 💎 ،")
+        stayle_pic.remove(message.chat.id)
+        return await message.reply_text("تم فتح امر صورتي بنجاح 💎 ،")
+    else:
+        return await message.reply_text("عذرا عزيزي هذا الامر للمشرفين بس 💎 ،")
+
+@app.on_message(filters.command(["صورتي","ص"], ""))
+async def muid(client: Client, message):
+    if message.chat.id in stayle_pic:
+        return await message.reply_text("صورتي معطل اطلب من المشرفين تفتحه 💎 ،")
     usr = await client.get_users(message.from_user.id)
     name = usr.first_name
     async for photo in client.get_chat_photos(message.from_user.id, limit=1):
-                    await message.reply_photo(photo.file_id,       caption=f"""صورتك {choice(RAEAK)} 🐉""", 
-        reply_markup=InlineKeyboardMarkup(
+                    await message.reply_photo(photo.file_id,       caption=f"""{choice(The_Stayle_Pic)}  💎 ،""", 
+         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        name, url=f"https://t.me/{message.from_user.username}")
-                ],
+                        name, url=f"https://t.me/{usr.username}")
+                ],[
+                    InlineKeyboardButton(
+                        "‹ 𖥳 𝙲𝙷𝙰𝙽𝙽𝙴𝙻 𖥳 ›", url=f"https://t.me/SOURCEOLIVEA")
+                ]
             ]
         ),
     )
-
-EMOJIS = list("😭🤣😂😅😆😁😄😃😀🥳🤩🤩😍🥰😘😚😙😗😉🤪😜😝😛😋🥲🙂🙃😶😐😑🫣🤭")
-@app.on_message(filters.regex("^الاسرع$") & filters.group)
-@app.on_edited_message(filters.regex("^الاسرع$") & filters.group)
-async def game_1(client, message):
-   emoji = random.choice(EMOJIS)
-   re = f"^{emoji}$"
-   ASK = await app.ask(
-     message.chat.id,
-     "اسرع واحد يرسل الايموجي : `{}`".format(emoji),
-     reply_to_message_id=message.id,
-     filters=filters.regex(re)
-   )
-   await app.send_message(
-      message.chat.id,
-      "المستخدم {} كفو اجابتك صح".format(ASK.from_user.mention),
-      reply_to_message_id=ASK.id
-   )
-   
