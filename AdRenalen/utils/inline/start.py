2e98109e59471f22f55781b0d3decf717ee0da26
Mazen Said <155029461+Mazensaid24@users.noreply.github.com>
2024-05-13 18:31:00 +0300

    Update start.py

--- AdRenalen/utils/inline/start.py
+++ AdRenalen/utils/inline/start.py
@@ -8,18 +8,14 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="اضف البوت الي جروبك او قناتك 🎸 ⋅",
+                text="اضف البوت الي مجموعتك ✅ ⋅",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text="الاوامر 🎸 ⋅", callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text="الاوامر ⚙", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="المطور 🎸 ⋅", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="قناة السورس 🎸 ⋅", url=config.SUPPORT_CHANNEL),
-        ],
-        [
-         
-            InlineKeyboardButton(text="𓂄𓆩νιρ ʍαzεπ𓆪𓂁", url=f"https://t.me/ZzZzZl1l"),
+            InlineKeyboardButton(text="المطور", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="قناة السورس", url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
@@ -29,18 +25,14 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="اضف البوت الي جروبك او قناتك 🎸 ⋅",
+                text="اضف البوت الي مجموعتك ✅ ⋅",
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
-        [InlineKeyboardButton(text="الاوامر 🎸 ⋅", callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text="الاوامر ⚙", callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text="المطور 🎸 ⋅", user_id=config.OWNER_ID),
-            InlineKeyboardButton(text="قناة السورس 🎸 ⋅", url=config.SUPPORT_CHANNEL),
-        ],
-        [
-         
-            InlineKeyboardButton(text="𓂄𓆩νιρ ʍαzεπ𓆪𓂁", url=f"https://t.me/ZzZzZl1l"),
+            InlineKeyboardButton(text="المطور ⋅", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="قناة السورس ⋅", url=config.SUPPORT_CHANNEL),
         ],
     ]
     return buttons
